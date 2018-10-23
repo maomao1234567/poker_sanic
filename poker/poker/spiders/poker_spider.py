91fadb7c35c18de97d87828923edd282a312f08c
MAYA-MUYI <1278077260@qq.com>
2018-10-23 16:15:56 +0800

    取消注释

--- poker/poker/spiders/poker_spider.py
+++ poker/poker/spiders/poker_spider.py
@@ -12,35 +12,29 @@ class PokerSpiderSpider(scrapy.Spider):
     def parse(self, response):
 
         href_list = response.xpath('//div[@class="content_list"]/div/a/@href').extract()
-        # for list in href_list:
-        print(len(href_list))
-        new_list = []
         for list in href_list:
-            if list not in new_list:
-                new_list.append(list)
-        print(len(new_list))
-            #yield scrapy.Request(url="http://thepokerlogic.com"+list, callback=self.parse_tag)
+            yield scrapy.Request(url="http://thepokerlogic.com"+list, callback=self.parse_tag)
 
-    # def parse_tag(self, response):
-    #     #     item = PokerItem()
-    #     #     item['title'] = response.xpath('//div[@class="glossary-detail"]/h2/text()').extract_first()
-    #     #     item['describe_title'] = response.xpath('//div[@class="detail-describe"]/h3/text()').extract_first()
-    #     #     item['describe_content'] = response.xpath('normalize-space(//div[@class="describe-content"]/p/text())').extract_first()
-    #     #     item['illustrate_title'] = response.xpath('//div[@class="detail-illustrate"]/h4/text()').extract_first()
-    #     #     item['illustrate_content'] = response.xpath('//div[@class="illustrate-content"]/p/text()').extract_first()
-    #     #     item['relevant_title'] = response.xpath('//div[@class="detail-relevant"]/h4/text()').extract_first()
-    #     #     relevant_content_a = response.xpath('//div[@class="relevant-content"]/a/text()').extract_first()
-    #     #     relevant_content_p = response.xpath('//div[@class="relevant-content"]/p/text()').extract_first()
-    #     #     if not relevant_content_a:
-    #     #         item['relevant_content'] = relevant_content_p
-    #     #     else:
-    #     #         item['relevant_content'] = relevant_content_a
-    #     #
-    #     #     item['study_title'] = response.xpath('//div[@class="detail-study"]/h4/text()').extract_first()
-    #     #     study_content_a = response.xpath('//div[@class="study-content"]/a/text()').extract()
-    #     #     study_content_p = response.xpath('//div[@class="study-content"]/p/text()').extract()
-    #     #     if not study_content_a:
-    #     #         item['study_content'] = study_content_p
-    #     #     else:
-    #     #         item['study_content'] = study_content_a
-    #     #     return item
+    def parse_tag(self, response):
+            item = PokerItem()
+            item['title'] = response.xpath('//div[@class="glossary-detail"]/h2/text()').extract_first()
+            item['describe_title'] = response.xpath('//div[@class="detail-describe"]/h3/text()').extract_first()
+            item['describe_content'] = response.xpath('normalize-space(//div[@class="describe-content"]/p/text())').extract_first()
+            item['illustrate_title'] = response.xpath('//div[@class="detail-illustrate"]/h4/text()').extract_first()
+            item['illustrate_content'] = response.xpath('//div[@class="illustrate-content"]/p/text()').extract_first()
+            item['relevant_title'] = response.xpath('//div[@class="detail-relevant"]/h4/text()').extract_first()
+            relevant_content_a = response.xpath('//div[@class="relevant-content"]/a/text()').extract_first()
+            relevant_content_p = response.xpath('//div[@class="relevant-content"]/p/text()').extract_first()
+            if not relevant_content_a:
+                item['relevant_content'] = relevant_content_p
+            else:
+                item['relevant_content'] = relevant_content_a
+
+            item['study_title'] = response.xpath('//div[@class="detail-study"]/h4/text()').extract_first()
+            study_content_a = response.xpath('//div[@class="study-content"]/a/text()').extract()
+            study_content_p = response.xpath('//div[@class="study-content"]/p/text()').extract()
+            if not study_content_a:
+                item['study_content'] = study_content_p
+            else:
+                item['study_content'] = study_content_a
+            return item
