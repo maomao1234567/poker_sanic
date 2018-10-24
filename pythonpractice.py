0ba6fe2ab32da80026e761372c670be76753270c
kongxin <1095716578@qq.com>
2018-10-24 20:39:47 +0800

    del

--- pythonpractice.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-r = requests.get('https://zh.pokerstrategy.com/glossary').content
-resolver = BeautifulSoup(r, 'html.parser')
-links = resolver.findAll('a')
-for link in links:
-    print(link)
-
