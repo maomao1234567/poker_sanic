846625caf5510e643401be3800b42e5b8058d201
leisongbai <2574162899@qq.com>
2018-11-13 21:04:23 +0800

    pokerspider

---
+++ poker_item/poker_item/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PokerItemItem(scrapy.Item):
+    # 定义item的字段
+    name = scrapy.Field()
+    body = scrapy.Field()
